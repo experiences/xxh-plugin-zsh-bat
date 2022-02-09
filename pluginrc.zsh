407c69c1285550afc9fb40e9748289ba6e2da794
experience <m9experiene@gmail.com>
2022-02-09 15:11:42 +0800

    udpate

--- pluginrc.zsh
+++ pluginrc.zsh
@@ -7,4 +7,5 @@ CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
 plugin_name='xxh-plugin-zsh-bat'
 
 export PATH=$CURR_DIR/bat:$PATH
-alias cat='bat'
+export BAT_THEME="Solarized (light)"
+alias cat='bat --paging=never'
