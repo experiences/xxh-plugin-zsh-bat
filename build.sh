bd8d4297368fc31b0d61de7b2d639d08117f5ba3
experience <m9experiene@gmail.com>
2022-02-09 14:49:33 +0800

    first commit

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+CDIR="$(cd "$(dirname "$0")" && pwd)"
+build_dir=$CDIR/build
+
+while getopts A:K:q option
+do
+  case "${option}"
+  in
+    q) QUIET=1;;
+    A) ARCH=${OPTARG};;
+    K) KERNEL=${OPTARG};;
+  esac
+done
+
+rm -rf $build_dir
+mkdir -p $build_dir
+
+for f in pluginrc.zsh
+do
+    cp $CDIR/$f $build_dir/
+done
+
+portable_url='https://github.com/sharkdp/bat/releases/download/v0.19.0/bat-v0.19.0-x86_64-unknown-linux-musl.tar.gz'
+tarname=`basename $portable_url`
+
+cd $build_dir
+
+[ $QUIET ] && arg_q='-q' || arg_q=''
+[ $QUIET ] && arg_s='-s' || arg_s=''
+[ $QUIET ] && arg_progress='' || arg_progress='--show-progress'
+
+if [ -x "$(command -v wget)" ]; then
+ wget $arg_q $arg_progress $portable_url -O $tarname
+elif [ -x "$(command -v curl)" ]; then
+ curl $arg_s -L $portable_url -o $tarname
+else
+ echo Install wget or curl
+fi
+
+tar -xzf $tarname
+rm $tarname
---
+++ pluginrc.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This script will be executed ON THE HOST when you connect to the host.
+# Put here your functions, environment variables, aliases and whatever you need.
+
+CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
+plugin_name='xxh-plugin-zsh-bat'
+
+export PATH=$CURR_DIR:$PATH
+alias cat='bat'
